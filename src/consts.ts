a592e3ab6469a8747cc52a0a60df42bfcad5136a
Notenlish <71970100+Notenlish@users.noreply.github.com>
2026-06-23 11:24:45 +0300

    change site description on consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'pygame-ce';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Homepage of the Pygame-ce project!';
